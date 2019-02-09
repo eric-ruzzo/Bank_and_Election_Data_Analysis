17a007bc69b9b841b6092e133d4e5c7ce5a33522
Ruzzo <eric.ruzzo@gmail.com>
2019-02-09 12:43:27 -0500

    Finished PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,7 @@ import os
 import csv
 
 # Path to collect data from folder
-pybankCSV = os.path.join("..", "PyBank", "budget_data.csv")
+pybankCSV = os.path.join("..", "BackupFiles", "budget_data.csv")
 
 # Print title
 print("Financial Analysis")
@@ -28,19 +28,14 @@ with open(pybankCSV, "r", newline="") as csvfile:
         if len(profit_loss) > 1:
             difference_list.append(profit_loss[-1] - profit_loss[-2])
 
-    # Assign variables to print
+    # Determine total number of months and print total
     months_total = len(months)
     total_profit = sum(profit_loss)
-    changes = sum(difference_list)
-    average = round(changes / (months_total - 1), 2)
-    greatest_increase = max(difference_list)
-    greatest_decrease = min(difference_list)
-
+    change = sum(difference_list)
         
     print(f"Total Months: {months_total}")
     print(f"Total Profit/Loss: ${total_profit}")
-    print(f"Average Change: ${average}")
-    print(f"Greatest Increase in Profits: {months[difference_list.index(greatest_increase) + 1]} (${greatest_increase})")
-    print(f"Greatest Decrease in Profits: {months[difference_list.index(greatest_decrease) + 1]} (${greatest_decrease})")
-
-# Not sure how to write lol
+    print(f"Average Change: ${change / (months_total - 1)}")
+    print(f"Greatest Increase in Profits: {months[difference_list.index(max(difference_list)) + 1]} (${max(difference_list)})")
+    print(f"Greatest Decrease in Profits: {months[difference_list.index(min(difference_list)) + 1]} (${min(difference_list)})")
+    
